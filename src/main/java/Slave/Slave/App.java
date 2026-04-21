edbb511ec53be9ce7227c22d540ed8ed61395088
AmarnathBalotagi <you@example.com>
2026-04-21 12:23:59 +0530

    Resume Uploader

---
+++ src/main/java/Slave/Slave/App.java
@@ -0,0 +1,13 @@
+package Slave.Slave;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Major_Slave/Naukri_Resumetest.java
@@ -0,0 +1,62 @@
+package Major_Slave;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class Naukri_Resumetest {
+	
+	@Test
+	public void Resume_Upload() throws AWTException {
+		WebDriver driver = new ChromeDriver();
+		 Actions act = new Actions(driver);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.naukri.com/");
+		WebElement Login = driver.findElement(By.id("login_Layer"));
+		Login.click();
+		WebElement UN = driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"));
+		UN.sendKeys("amarnath.s.b123@gmail.com");
+		WebElement PWD = driver.findElement(By.xpath("//input[@placeholder='Enter your password']"));
+		PWD.sendKeys("amartheboss");
+		WebElement Login_btn = driver.findElement(By.xpath("//button[@class='btn-primary loginButton']"));
+		Login_btn.click();
+		act.moveByOffset(100, 200).click().perform();   
+		WebElement ViewProfile = driver.findElement(By.xpath("//a[text()='View']"));
+		ViewProfile.click();
+		WebElement Updateresume = driver.findElement(By.xpath("//input[@value='Update resume']"));
+		js.executeScript("arguments[0].scrollIntoView({block:'center'});", Updateresume);
+		Updateresume.click();
+		
+		 StringSelection ss	= new StringSelection("C:\\Users\\Amarnath\\eclipse-workspace\\Selenium_Prep\\Resume\\Amarnath_QA_3.2 YOE.pdf");
+		 Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		 Robot rb = new Robot();
+		 rb.delay(2000);
+		 rb.keyPress(KeyEvent.VK_CONTROL);
+		 rb.keyPress(KeyEvent.VK_V);
+		 
+		 rb.keyRelease(KeyEvent.VK_CONTROL);
+		 rb.keyRelease(KeyEvent.VK_V);
+		 
+		 rb.keyPress(KeyEvent.VK_ENTER);   
+		 rb.keyRelease(KeyEvent.VK_ENTER);
+		 System.out.println("----------Resume has been Uploaded-----------");
+		 driver.quit();
+	}
+
+}
